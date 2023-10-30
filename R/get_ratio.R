eab57679080d668296a273715f3ef57de3ae6052
Tomer Iwan <t.iwan@vu.nl>
2023-10-30 11:38:39 +0100

    fix: wrong categories for other values

--- R/get_ratio.R
+++ R/get_ratio.R
@@ -1,16 +1,17 @@
-#' Get Ratio
+#' get_ratio
 #'
-#' This function gets the percentages for factor columns.
-#'
-#' @param data_vector Column to check.
+#' Get percentages for factor columns
+#' @param data_vector column to check
 #' @family assertions
 #' @family tests
 get_ratio <- function(data_vector) {
+
   # Initialize percent variable
   percent <- NULL
 
   # Check if the data_vector meets certain conditions
   if (!lubridate::is.POSIXct(data_vector)) {
+
     # Create frequency table of the data_vector and sort it by descending order
     frequency_table <- janitor::tabyl(data_vector) %>%
       dplyr::arrange(dplyr::desc(percent)) %>%
@@ -20,23 +21,23 @@ get_ratio <- function(data_vector) {
     # Assign the categories to a variable
     categories <- frequency_table$data_vector
 
-    # Append the string "Other" to the categories
-    categories <- append(categories, "Other")
-
     # Assign the percentages to a variable
     percentages <- format(frequency_table$percent * 100, digits = 2)
 
-    # Append the remaining percentage to the percentages
-    if (sum(as.numeric(percentages)) != 100) {
+    if (sum(as.numeric(percentages)) < 100) {
       percentages <- append(percentages, 100 - sum(as.numeric(percentages)))
+      categories <- append(categories, "Overig")
     } else {
-      categories <- categories[-1]
+      percentages[length(percentages)] <- 100 - sum(as.numeric(percentages[-length(percentages)]))
     }
+
 
     # Combines the categories and percentages into a single string
     result <- paste0(categories, ": (", percentages, "%)", collapse = " | ")
     return(result)
-  } else if (lubridate::is.POSIXct(data_vector)) {
+  }
+  else if (lubridate::is.POSIXct(data_vector)) {
+
     # Create frequency table of the data_vector and sort it by descending order and keep top 10 values
     frequency_table <- janitor::tabyl(data_vector) %>%
       dplyr::arrange(dplyr::desc(percent)) %>%
@@ -47,28 +48,28 @@ get_ratio <- function(data_vector) {
     # Assign the categories to a variable
     categories <- as.character(frequency_table$data_vector)
 
-    # Append the string "Other" to the categories
-    categories <- append(categories, "Other")
-
     # Assign the percentages to a variable
     percentages <- format(frequency_table$percent * 100, digits = 2)
 
     # Append the remaining percentage to the percentages
-    if (sum(as.numeric(percentages)) != 100) {
+    # Append the remaining percentage to the percentages
+    if (sum(as.numeric(percentages)) < 100) {
       percentages <- append(percentages, 100 - sum(as.numeric(percentages)))
+      categories <- append(categories, "Overig")
     } else {
-      categories <- categories[-1]
+      percentages[length(percentages)] <- 100 - sum(as.numeric(percentages[-length(percentages)]))
     }
 
     # Check if first percentage is less than 1
     if (as.numeric(percentages[1]) < 0.01) {
-      return("Not enough occurrences")
+      return("Not enough occurences")
     } else {
       # Combines the categories and percentages into a single string
       result <- paste0(categories, ": (", percentages, "%)", collapse = " | ")
       return(result)
     }
-  } else {
+  }
+  else{
     return("Too many categories")
   }
 }
